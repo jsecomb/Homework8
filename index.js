3ae971bce6cc9bc7f84f35c351573c0789ba86e2
Julian Secomb <hoodooprojects@gmail.com>
2020-04-10 16:05:11 -0700

    fixed filepath to markdown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const axios = require("axios");
-const generateMarkdown = require("./utils/generateMarkdown.js");
+const generateMarkdown = require("./generateMarkdown.js");
 
 inquirer
     .prompt([
